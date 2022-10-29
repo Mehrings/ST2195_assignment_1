c3b1c9c1306881f5a02a38a4f65431faf7874be2
Mehrings <116951094+Mehrings@users.noreply.github.com>
2022-10-29 20:48:00 +0800

    Add files via upload

---
+++ integers.R
@@ -0,0 +1,4 @@
+my_range <- 1:10
+for (i in my_range){
+  print(paste("The integer is", i))
+}
---
+++ spyder.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 29 19:36:13 2022
+
+@author: johnson
+"""
+
+for integer in range(1,11):
+    print(f"The integer is {integer}.")
+    
